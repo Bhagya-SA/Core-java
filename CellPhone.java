86690b5cc3cfce08f4a9925c0ff9604d232a74d5
Bhaya-SA <bhagyaanjaneya@gmail.com>
2026-02-17 21:48:39 +0530

    Use of conditional statements inside the method

---
+++ CellPhone.java
@@ -0,0 +1,267 @@
+class CellPhone {
+	public static long searchContactNumber(String contactName) {
+		long contactNo = 0L;
+		if(contactName == "Amma") {
+			contactNo = 6785439806L;
+			return contactNo;
+		}
+		else if(contactName == "Appaji") {
+			contactNo = 7894563789L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Dodamma") {
+			contactNo = 6578904536L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Dodappaji") {
+			contactNo = 987456302L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Chaitrakka") {
+			contactNo = 8974563902L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Chethanna") {
+			contactNo = 8902345674L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Bushanna") {
+			contactNo = 987234501L;
+			return contactNo;
+		}
+		else if(contactName == "Bharathanna") {
+			contactNo = 8762341098L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Deepa Bhabi") {
+			contactNo = 9871234045L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Keerthi Bhabi") {
+			contactNo = 8763452091L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Anusha attige") {
+			contactNo = 7892341678L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Tejeshwini") {
+			contactNo = 8901234567L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Aadhya") {
+			contactNo = 9012345678L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Hima") {
+			contactNo = 8761234568L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Chandana") {
+			contactNo = 8901235674L;
+			return contactNo;
+		}
+		else if(contactName == "Chaitrakka") {
+			contactNo = 9082345671L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Ananya") {
+			contactNo = 78912345642L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Divya") {
+			contactNo = 9056723415L;
+			return contactNo;
+		}
+		else if(contactName == "Durga") {
+			contactNo = 6785432189L;
+			return contactNo;
+		}
+		else if(contactName == "Ganganna Sagar") {
+			contactNo = 78902234567L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Gowri Aunty") {
+			contactNo = 9123456789L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Jessy Miss") {
+			contactNo = 9087654323L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Kamalu aunty") {
+			contactNo = 9801236754L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Kavya Ma'am") {
+			contactNo = 7654321908L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Lakshmi") {
+			contactNo = 9345678021L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Lallu aunty") {
+			contactNo = 8901234567L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Latha Aunty") {
+			contactNo = 8976513452L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Manthu Anna") {
+			contactNo = 9654321906L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Manju Aunty") {
+			contactNo = 76548901234L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Manjuakka") {
+			contactNo = 7894567834L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Madhuri Ma'am") {
+			contactNo = 6543217890L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Lavanya R") {
+			contactNo = 7654321890L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Rama Ma'am") {
+			contactNo = 9877890987L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Raghu") {
+			contactNo = 7689067865L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Nittvali Dodamma") {
+			contactNo = 7865906578L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Meghanath Sir") {
+			contactNo = 9801233245L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Maruthi xeror") {
+			contactNo = 9789654321L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Sagar Anna") {
+			contactNo = 6787654567L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Ruthu") {
+			contactNo = 9012091232L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Rohini akka") {
+			contactNo = 9876540120L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Rekha") {
+			contactNo = 8769045363L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Vedha") {
+			contactNo = 8778989009L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Varsha") {
+			contactNo = 9023325665L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Srujana") {
+			contactNo = 9988776654L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Srujan") {
+			contactNo = 9103452189L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Spoorthi") {
+			contactNo = 8761233214L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Shreya") {
+			contactNo = 7650981234L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Shishir") {
+			contactNo = 9087789651L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Shifa") {
+			contactNo = 7655678790L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Savitramma Miss") {
+			contactNo = 8900987654L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Saraswathi") {
+			contactNo = 876689860L;
+			return contactNo;
+		
+		}
+		else if(contactName == "Sahana") {
+			contactNo = 9087689076L;
+			return contactNo;
+		
+		}
+		
+		else {
+			System.out.println("No contact available");
+		}
+		return contactNo;	
+		
+	}
+}
+
---
+++ CellPhoneRunner.java
@@ -0,0 +1,9 @@
+class CellPhoneRunner {
+	public static void main(String[] phone) {
+		
+		String name = "Varsha";
+		long contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+	
+	}
+}
