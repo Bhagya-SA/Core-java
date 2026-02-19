ecebd02d2daf3e21d50ac1e0269e59012f7d37e5
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-19 18:48:53 +0530

    Update CellPhoneRunner.java

--- CellPhoneRunner.java
+++ CellPhoneRunner.java
@@ -1,9 +1,213 @@
 class CellPhoneRunner {
 	public static void main(String[] phone) {
 		
-		String name = "Varsha";
+		String name = "Amma";
 		long contactNumber = CellPhone.searchContactNumber(name);
 		System.out.println(name + ":" + contactNumber);
+
+		 name = "Appaji";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Dodamma";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Dodappaji";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Chaitrakka";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Chethanna";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Bushanna";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Bharathanna";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Deepa Bhabi";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Keerthi Bhabi";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Anusha attige";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Tejeshwini";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Aadhya";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Hima";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Chandana";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Chaitrakka B";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Ananya";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Divya";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Durga";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Ganganna Sagar";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Gowri Aunty";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Jessy Miss";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Kamalu aunty";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Kavya Ma'am";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Lakshmi";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Lallu aunty";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Latha Aunty";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Manthu Anna";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Manju Aunty";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Manjuakka";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Madhuri Ma'am";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Lavanya R";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Rama Ma'am";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Raghu";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Nittvali Dodamma";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Meghanath Sir";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Maruthi xeror";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Sagar Anna";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Ruthu";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Rohini akka";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Rekha";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Vedha";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Varsha";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Srujana";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Srujan";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Spoorthi";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Shreya";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Shishir";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Shifa";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Savitramma Miss";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Saraswathi";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);
+
+		name = "Sahana";
+		 contactNumber = CellPhone.searchContactNumber(name);
+		System.out.println(name + ":" + contactNumber);	
 	
 	}
 }
