f641fb1bb9ec3a7cb21d3f7c2aefe05507afa2b0
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-19 18:21:47 +0530

    Update food items in FoodPandaExecutor

--- FoodPandaExecutor.java
+++ FoodPandaExecutor.java
@@ -2,10 +2,221 @@ class FoodPandaExecutor {
 	
 	public static void main(String[] foodPanda) {
 		
-		String foodName = "Loaded Nachos";
+		String foodName = "Tender Coconut Water";
 		double cost = FoodPanda.search(foodName);
-		
 		System.out.println("The price of " + foodName + " is : " + cost);
-	
+
+		foodName = "Coke";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Bottled Water";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Masala Chai";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Mango Smoothie";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Fresh Lime Soda";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Oreo Milkshake";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Cold Coffee";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Kulfi Faluda";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Vanilla Ice Cream";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Gulab Jamun";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Cheesecake";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chocolate Brownie";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Quinoa Salad";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Grilled Chicken Salad";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Protein Power Bowl";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Greek Salad";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Caesar Salad";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Pakoras";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Aloo Tikki";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Dahi Papdi Chaat";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Bhel Puri";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Gol Gappa";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Kachor";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Shawarma Wrap";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Mutton Seekh Roll";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Kathi Roll";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Rava Kesari";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Ghee Roast Dosa";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Dimsums";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Manchow Soup";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = " Hot and Sour Soup";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = " Chilli Garlic Fish";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Schezwan Noodles";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Spring Rolls";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Lollipop";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chilli Paneer";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Hakka Noodles";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Nuggets";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Veggie Supreme Pizza";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Loaded Nachos";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Veg Whopper";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Club Sandwich";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Paneer Do Pyaza";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Lachha Paratha";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Garlic Naan";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Mix Veg Curry";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Shahi Paneer";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Steamed Basmati Rice ";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Curd Rice";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Donne Biryani";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Hyderabadi Chicken Dum Biryan";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Dal Tadk";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Strawberry Ice cream";
+		cost = FoodPanda.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
 	}
 }
