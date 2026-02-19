982435d52f1ef1e5ccd478494d7ca90ffe9ba4ec
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-19 18:29:27 +0530

    Update food items in ZeptoExecutor

--- ZeptoExecutor.java
+++ ZeptoExecutor.java
@@ -1,12 +1,222 @@
 class ZeptoExecutor {
 
 	public static void main(String[] zepto) {
-		
-		String foodName = "Strawberry Lemonade";
-		double cost = Zepto.search(foodName);
-		
-		System.out.println("The price of " + foodName + " is : " + cost);
-	
-	}
 
+		String foodName = "Vada Pav";
+		double cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Samosa Pav";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Cheese Maggi";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Plain Maggi";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Veg Puff";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Puff";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Bombay Aloo Tikki Sandwich";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chilli Cheese Toast";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Garlic Bread with Cheese Dip";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Garlic Bread Party Pack";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Butter Chicken Steamed Bao";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Veg Steamed Pizza Bao";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Mini Butter Croissants";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Almond Croissant";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Butter Croissant";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chole Kulche";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Paneer Makhani & Rice";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Butter Chicken & Rice";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Dal Makhani & Rice";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Rajma Masala & Rice";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Medu Vada Sambar Dip";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Rawa Upma";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Poha";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Masala Omelette Pav";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Vietnamese Cold Coffee";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Adrak Chai";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Hot Chocolate";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "French Vanilla Hot Chocolate";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Strawberry Shake";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Mixed Berry Shake";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Strawberry Lemonade";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Masala Chaas";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Triple Chocolate Mousse";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Kesari Rasmalai";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Tiramisu";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Angoori Gulab Jamun";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Choco Lava Cake";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Hot Americano";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Hot Milk";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "McCain French Fries";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "McCain Masala Fries";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "McCain Veggie Nuggets";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "McCain Chilli Chicken Sausages";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Prasuma Momos (Veg/Chicken";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "ITC Master Chef Chicken Seekh Kebab";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Venky's Chicken Nuggets";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Switz Paratha Puff";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Wow! Chicken Tikka Biryani";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Meatzza Chicken Jumbo Hotdog";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Frozen Green Peas";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Frozen Sweet Corn";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Frozen Edamame";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Frozen Diced Mango";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Smoked Paneer";
+		cost = Zepto.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+	}
 }
