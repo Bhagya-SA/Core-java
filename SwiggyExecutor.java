e04ecdb2dcda7164ab186eeeb54513306e92045b
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-19 18:23:53 +0530

    Add multiple food item price checks in SwiggyExecutor

--- SwiggyExecutor.java
+++ SwiggyExecutor.java
@@ -1,11 +1,222 @@
 class SwiggyExecutor {
 
 	public static void main(String[] swiggy) {
-	String foodName = "Schezwan Fried Rice";
-	double cost = Swiggy.search(foodName);
 		
-	System.out.println("The price of " + foodName + " is : " + cost);
-	
-	}
+		String foodName = "Paneer Butter Masala";
+		double cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
 
+		foodName = "Kadhai Chicken";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Kolhapuri";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Dal Tadka";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Dal Makhani";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Palak Chicken";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "French Fries";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Methi Chicken";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Roti/Naan";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Veg Platter";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Kebabs";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Murgh Koobideh";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Idli";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Medhu Vada";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Pongal";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Veg Puri";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Aloo Parantha";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Onion Dosa";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Rava Dosa";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Set Dosa";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Uttapam";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Momos";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Veg Momos";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Samosa";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Adrak Chai";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Paneer Rolls";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Tikka Roll";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Falafel";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Peri Peri Fries";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "White Chocolate Cake";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chocolate Cake";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Gulab Jamun";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Kaju Barfi";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Besan Ladoo";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chocolate Pastry";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Dark Chocolate Ice Cream";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chocolate Sundae";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Rasgulla";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Kunafa";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Mexican Tacos";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Korean Fried Chicken";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Thai Green Curry";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chilli Chicken";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Tikki Burger";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Veg Maharaja Burger";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Cheese Garlic Bread";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Paneer Pizza";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Pepperoni Pizza";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Thin Crust Pizza";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Double Cheese Burger";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Nuggets";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Schezwan Fried Rice";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Fries (Potato)";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Onion Rings";
+		cost = Swiggy.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+	}
 }
