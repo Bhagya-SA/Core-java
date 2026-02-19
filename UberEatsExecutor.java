8d5965300c21aa4d489c0f27852a544fc77e2aa8
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-19 18:26:44 +0530

    Expand food search options in UberEatsExecutor

--- UberEatsExecutor.java
+++ UberEatsExecutor.java
@@ -1,12 +1,226 @@
 class UberEatsExecutor {
 
 	public static void main(String[] uberEats) {
-	String foodName = "Garlic Bread";
-	double cost = UberEats.search(foodName);
 		
-	System.out.println("The price of " + foodName + " is : " + cost);
-	
-	}
+		String foodName = "Burrito bowl";
+		double cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
 
+		foodName = "Chicken nuggets";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken sandwich";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Waffle fries";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Cheeseburger";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Mac & cheese";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Boneless wings";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Pepperoni pizza";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Fried rice ";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Shack Burger";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Double Smashed Burger";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Kyiv Sandwich";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "BBQ Beef Brisket Sandwich";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "The Hogless Roast Burger";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Gyro";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Double Down Burger";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Classic Bacon Cheeseburger";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Philly Cheesesteak";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Grilled Chicken Caesar Wrap";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Fish Sandwich";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "BLT Sandwich";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = " Veggie Burger";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Meatball Sub";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Pulled Pork Sandwich ";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Margherita Pizza";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Meat Lovers Pizza";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Garlic Knots";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Carbonara";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Spaghetti and Meatballs";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Lasagna";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Parmesan";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = " Penne alla Vodka";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Cheese Pizza";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = " Stromboli";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Calzone";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Fettuccine Alfredo";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Garlic Bread";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Pesto Pasta";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Baked Ziti ";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Pad Thai";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chilli Chicken Ramen";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Prawn & Pork Spicy Wonton";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Pan Fried Pork Soup Dumplings";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "General Tso's Chicken";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Sweet & Sour Pork";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Char Kuey Teow";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = " Sushi Combo";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Orange Chicken";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Vegetable Spring Rolls";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Masala Dosa";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Tikka Masala";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Pad See Ew";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = " Green Curry";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Tom Yum Soup";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Wonton Soup";
+		cost = UberEats.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+	}
 }
-	
