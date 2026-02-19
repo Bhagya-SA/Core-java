5a00dd85ece3d4a3a3f8b4d7e2755be84aa43a7f
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-19 18:10:31 +0530

    Change foodNames from 'Pringles' to 'Onions' and displaying the price of each foods

--- BlinKitExecutor.java
+++ BlinKitExecutor.java
@@ -2,9 +2,220 @@ class BlinKitExecutor {
 
 	public static void main(String[] blinkit) {
 		
-		String foodName = "Pringles";
+		String foodName = "Onions";
 		double cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
 		
+		foodName = "Potatoes";
+	     cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Tomatoes";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Coconut";
+		cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Green Chilli";
+		cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Garlic";
+		cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Coriander Leaves";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Curry Leaves";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Lemon";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Cucumber";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Capsicum";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Carrot";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Beans";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Spinach";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Broccoli";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Mushrooms";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Apple";
+		cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Oranges";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Pomegranate";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Watermelon";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Grapes";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Mango";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Pre-cut Fruit Pack";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Amul Milk";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Mother Dairy Curd";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Britannia Bread";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Amul Butter";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Cheese Slices";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Mozzarella Cheese";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Yogurt";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Fresh Cream";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Condensed Milk";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Sliced Pav";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Bun";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Cream Rolls";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Sandwich Bread";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Lay’s India’s Magic Masala";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Kurkure Masala Munch";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Haldiram's Aloo Bhujia";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Haldiram's Bhujia Sev";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Haldiram's Punjabi Tadka";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Bikano Navratan Mixture";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Pringles";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Bingo! Mad Angles";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Pringles";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Kettle Studio Chips";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Cornitos Nachos";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Parle-G Biscuits";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Britannia Good Day";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Oreo Cookies";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Hide & Seek Chocolate Chips";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Marie Gold Biscuits";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Britannia NutriChoice Digestive";
+		 cost = BlinKit.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+		
+		foodName = "Trail Mix";
+		 cost = BlinKit.search(foodName);
 		System.out.println("The price of " + foodName + " is : " + cost);
 	
 	}
