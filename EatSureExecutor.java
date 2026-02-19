e7f83714806e08b279084f36c242761ba9564e58
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-19 18:15:53 +0530

    Update food items and prices in EatSureExecutor

--- EatSureExecutor.java
+++ EatSureExecutor.java
@@ -2,10 +2,225 @@ class EatSureExecutor {
 
 	public static void main(String[] eatSure) {
 		
-		String foodName = "Matar Paneer Meal";
+		String foodName = "Lazeez Bhuna Murgh Biryani";
 		double cost = EatSure.search(foodName);
-		
 		System.out.println("The price of " + foodName + " is : " + cost);
-	
+
+		foodName = "Dum Gosht Biryani";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Zaikedaar Paneer Biryani";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Murgh Koobideh Biryani";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Subz-e-Biryani (Veg)";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Tikka Biryani";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Keema Biryani";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Egg Biryani";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Mutton Seekh Biryani";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Paneer Tikka Biryani";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Kathal (Jackfruit) Biryani";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Makhani Biryani";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Shahi Veg Biryani";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Special 1kg Handi Biryan";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Tikka Roll";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Cheese Chicken Roll";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Double Chicken Max Roll";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Paneer Tikka Roll";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Veggie Wrap";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Egg Roll";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Double Paneer Roll";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Soya Tikka Roll";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Bhuna Roll";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Mutton Special Rol";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Peri Peri Chicken Roll";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Masala Paneer Roll";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Shawarma Roll";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Cheese Potato Rol";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Veggie Loaded Roll";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Cheese Burst Margherita";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Veggie Supreme Pizza";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Pepperoni Pizza";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Smoky BBQ Chicken Pizza";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Double Cheese Margherita";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Spicy Triple Chicken Pizza";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Farmhouse Veggie Pizza";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Thin Crust Pizza";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Dragonfire Chicken Pizza";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Cheese Burst Chicken Pizza";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Paneer Makhani Pizza";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "North Indian Chicken Thal";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "North Indian Chicken Thali";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Shahi Paneer Thali";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Dal Makhani Rice Bowl";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Curry Rice Bowl";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Rajma Rice Bowl";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Paneer Tikka Masala Meal";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chole Chawal";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Butter Chicken Meal";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Veg Fried Rice & Manchurian Bowl";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Chicken Kheema Rice Bowl";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Pindi Chole Meal";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Executive Veg Meal";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Egg Curry Rice Bowl";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
+		foodName = "Matar Paneer Meal";
+		cost = EatSure.search(foodName);
+		System.out.println("The price of " + foodName + " is : " + cost);
+
 	}
 }
