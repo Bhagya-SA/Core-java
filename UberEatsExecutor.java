ef19a3ee0f5e391c053a5d4c5bd70f54ef82b5fd
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-23 18:51:29 +0530

    Update food price output with quantity and total cost

--- UberEatsExecutor.java
+++ UberEatsExecutor.java
@@ -4,223 +4,333 @@ class UberEatsExecutor {
 		
 		String foodName = "Burrito bowl";
 		double cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		int quantity = 2;
+		double totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken nuggets";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken sandwich";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 4;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Waffle fries";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 5;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Cheeseburger";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Mac & cheese";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Boneless wings";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Pepperoni pizza";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 9;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Fried rice ";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 10;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Shack Burger";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 11;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Double Smashed Burger";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 12;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Kyiv Sandwich";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 13;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "BBQ Beef Brisket Sandwich";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 14;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "The Hogless Roast Burger";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 15;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Gyro";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 16;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Double Down Burger";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 17;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Classic Bacon Cheeseburger";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 18;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Philly Cheesesteak";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 19;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Grilled Chicken Caesar Wrap";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 20;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Fish Sandwich";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 21;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "BLT Sandwich";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 22;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = " Veggie Burger";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 23;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Meatball Sub";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 24;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Pulled Pork Sandwich ";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 25;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Margherita Pizza";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 26;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Meat Lovers Pizza";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 55;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Garlic Knots";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 54;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Carbonara";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 53;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Spaghetti and Meatballs";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 52;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Lasagna";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 51;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Parmesan";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 50;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = " Penne alla Vodka";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 49;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Cheese Pizza";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 48;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = " Stromboli";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 47;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Calzone";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 46;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Fettuccine Alfredo";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 45;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Garlic Bread";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 44;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Pesto Pasta";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 43;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Baked Ziti ";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 42;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Pad Thai";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 41;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chilli Chicken Ramen";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
-
+		quantity = 40;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 		foodName = "Prawn & Pork Spicy Wonton";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 39;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Pan Fried Pork Soup Dumplings";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
-
+		quantity = 38;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 		foodName = "General Tso's Chicken";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 37;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Sweet & Sour Pork";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 36;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Char Kuey Teow";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 35;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = " Sushi Combo";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
-
+		quantity = 35;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 		foodName = "Orange Chicken";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Vegetable Spring Rolls";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 33;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Masala Dosa";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 32;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Tikka Masala";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 31;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Pad See Ew";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 30;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = " Green Curry";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 29;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Tom Yum Soup";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 28;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Wonton Soup";
 		cost = UberEats.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 27;
+		totalCost = UberEats.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 	}
 }
