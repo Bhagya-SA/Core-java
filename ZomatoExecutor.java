43c51273d54f104e1f9f7d90f0d2c8de27603772
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-23 18:53:34 +0530

    Add quantity and total cost calculations for food items

--- ZomatoExecutor.java
+++ ZomatoExecutor.java
@@ -4,223 +4,334 @@ class ZomatoExecutor {
 
 		String foodName = "Sandwich";
 		double cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		int quantity = 2;
+		double totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 
 		foodName = "Burger";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 50;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Pizza";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 49;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Gobi Manchurian";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 48;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Panner tikka";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 47;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Noodles";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 46;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "French Fries";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 45;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Pasta";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 44;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Masal puri";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 43;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Vada sambhar";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 42;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Mix Uttapam";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 41;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Sev Barfi";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 40;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Pista Halwa";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 39;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Cold Coffee";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 38;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chai";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 37;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Biryani";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 36;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chole Bhature";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 36;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Idli Sambhar";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 35;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Dal Pakwaan";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Sindhi curry";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 33;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Butter Chicken";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 32;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Matar Paneer";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 31;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken rice bowl";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 30;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken curry";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 2;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Tandori Chicken";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 29;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Biryani";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 28;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Ice cream";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 27;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Crunchy Chicken";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 26;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Classic Chicken";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 25;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Double Classic Chicken burgers";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 24;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Farm Fresh Veggies";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 23;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Paneer Wrap";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 22;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Pops";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 21;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Paneer";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 20;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Double Classic Veg";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 19;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Classic Veg";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 18;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Hot Chicken Wings";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 2;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "BBQ Chicken Wings";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 17;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Strips";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 16;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Jumbo Chicken";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 15;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken wings";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 14;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Popcorn";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 14;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken fries";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 2;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Mangalorean Ghee Roasts";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 13;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chilli Broccoli";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 11;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Kesari Paneer Tikka";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 12;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Coal grilled Mushroom";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 10;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Paneer Lahori Tikka";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 9;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Bakarwadi";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Rice Flakes Chivda";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Bhajani Chakali";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Pan Mukhwas";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 5;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Pachak Plus";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 4;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Mukhwas";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Digestive Mix";
 		cost = Zomato.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 2;
+		totalCost = Zomato.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 	}
 }
