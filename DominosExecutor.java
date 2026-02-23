743cbe3ae82fdd5e9fd7e3dd6ed8da5404e44231
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-23 18:46:03 +0530

    Update food quantity and total cost calculations

--- DominosExecutor.java
+++ DominosExecutor.java
@@ -4,123 +4,186 @@ class DominosExecutor {
 		
 		String foodName = "Veg Margherita";
 		double cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		int quantity = 2;
+		double totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Onion Pizza";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 4;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Capsicum Pizza";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Golden Corn Pizza";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 		
 		foodName = "Paneer & Onion Pizza";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Veg Loaded Pizza";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 5;
+	    totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Non-Veg Loaded Pizza";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 		
 		foodName = "Veg Spicy";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Non-Veg Spicy";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 		
 		foodName = "Veg Herby";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 9;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Non-Veg Herby";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		 totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Creamy Tomato Pasta Pizza Veg";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Creamy Tomato Pasta Pizza Non-Veg";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Moroccan Spice Pasta Pizza Veg";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Moroccan Spice Pasta Pizza Non-Veg ";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "classic Margherita";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 2;
+		 totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Double Cheese Margherita";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 2;
+		 totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Paneer Makhani";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 2;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Indi Tandoori Paneer";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		 totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Pepper Barbecue Chicken";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Chicken Golden Delight";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 9;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Indi Chicken Tikka";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Garlic Breadstick";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 5;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Chicken Pepperoni";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Roasted Chicken Wings";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 4;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "White Pasta Italiano";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 4;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Choco Lava Cake";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 2;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Pepsi";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 38;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Sprite";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 20;
+		totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Large lava Pizza";
 		cost = Dominos.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 78;
+		 totalCost = Dominos.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 	}
 }
