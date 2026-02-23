9ee8cc4fa79866ff673cf0d98d3c931286a112e8
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-23 18:49:12 +0530

    Refactor FoodPandaExecutor to include quantity and total cost

--- FoodPandaExecutor.java
+++ FoodPandaExecutor.java
@@ -4,219 +4,328 @@ class FoodPandaExecutor {
 		
 		String foodName = "Tender Coconut Water";
 		double cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		int quantity = 3;
+		double totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Coke";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 
 		foodName = "Bottled Water";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 4;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Masala Chai";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 5;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Mango Smoothie";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Fresh Lime Soda";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Oreo Milkshake";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Cold Coffee";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Kulfi Faluda";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Vanilla Ice Cream";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 10;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Gulab Jamun";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 11;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Cheesecake";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 12;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chocolate Brownie";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Quinoa Salad";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 17;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Grilled Chicken Salad";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 15;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Protein Power Bowl";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 15;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Greek Salad";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 25;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Caesar Salad";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 4;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Pakoras";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 9;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Aloo Tikki";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 32;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Dahi Papdi Chaat";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 20;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Bhel Puri";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 29;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Gol Gappa";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 11;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Kachor";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 22;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Shawarma Wrap";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 35;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Mutton Seekh Roll";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 21;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Kathi Roll";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Rava Kesari";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Ghee Roast Dosa";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Dimsums";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Manchow Soup";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = " Hot and Sour Soup";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = " Chilli Garlic Fish";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 4;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Schezwan Noodles";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Spring Rolls";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 15;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Lollipop";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chilli Paneer";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Hakka Noodles";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Nuggets";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Veggie Supreme Pizza";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Loaded Nachos";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 10;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Veg Whopper";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 9;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Club Sandwich";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Paneer Do Pyaza";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Lachha Paratha";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Garlic Naan";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Mix Veg Curry";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Shahi Paneer";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Steamed Basmati Rice ";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Curd Rice";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Donne Biryani";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity =5;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Hyderabadi Chicken Dum Biryan";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 43;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Dal Tadk";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 4;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Strawberry Ice cream";
 		cost = FoodPanda.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = FoodPanda.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 	}
 }
