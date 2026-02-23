dfae72bbc33e269b4b9e5d3356af5edead8f8a3b
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-23 18:47:31 +0530

    Update food price display with quantities and totals

--- EatSureExecutor.java
+++ EatSureExecutor.java
@@ -4,203 +4,305 @@ class EatSureExecutor {
 		
 		String foodName = "Lazeez Bhuna Murgh Biryani";
 		double cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		int quantity = 2;
+		double totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Dum Gosht Biryani";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 
 		foodName = "Zaikedaar Paneer Biryani";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 4;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 
 		foodName = "Murgh Koobideh Biryani";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 5;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Subz-e-Biryani (Veg)";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Tikka Biryani";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Keema Biryani";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Egg Biryani";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 9;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Mutton Seekh Biryani";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Paneer Tikka Biryani";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 9;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Kathal (Jackfruit) Biryani";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Makhani Biryani";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Shahi Veg Biryani";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Special 1kg Handi Biryan";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Tikka Roll";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 10;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Cheese Chicken Roll";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Double Chicken Max Roll";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 30;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Paneer Tikka Roll";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Veggie Wrap";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 20;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Egg Roll";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Double Paneer Roll";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 10;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Soya Tikka Roll";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 54;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Bhuna Roll";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Mutton Special Rol";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 9;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Peri Peri Chicken Roll";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 5;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Masala Paneer Roll";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Shawarma Roll";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 62;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Cheese Potato Rol";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 6;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Veggie Loaded Roll";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 30;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Cheese Burst Margherita";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 35;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Veggie Supreme Pizza";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Pepperoni Pizza";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Smoky BBQ Chicken Pizza";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Double Cheese Margherita";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Spicy Triple Chicken Pizza";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Farmhouse Veggie Pizza";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 37;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Thin Crust Pizza";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 35;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Dragonfire Chicken Pizza";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Cheese Burst Chicken Pizza";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Paneer Makhani Pizza";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "North Indian Chicken Thal";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "North Indian Chicken Thali";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Shahi Paneer Thali";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 9;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Dal Makhani Rice Bowl";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Curry Rice Bowl";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Rajma Rice Bowl";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 5;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Paneer Tikka Masala Meal";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chole Chawal";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 24;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Butter Chicken Meal";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 23;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Veg Fried Rice & Manchurian Bowl";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chicken Kheema Rice Bowl";
 		cost = EatSure.search(foodName);
@@ -208,19 +310,27 @@ class EatSureExecutor {
 
 		foodName = "Pindi Chole Meal";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Executive Veg Meal";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Egg Curry Rice Bowl";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 31;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Matar Paneer Meal";
 		cost = EatSure.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 32;
+		totalCost = EatSure.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 	}
 }
