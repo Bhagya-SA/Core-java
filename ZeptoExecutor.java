24faa9e124660c6ec3303b8bf5bcc3f7bff66ef8
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-23 18:52:31 +0530

    Update food price display with quantities

--- ZeptoExecutor.java
+++ ZeptoExecutor.java
@@ -4,219 +4,326 @@ class ZeptoExecutor {
 
 		String foodName = "Vada Pav";
 		double cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		int quantity = 2;
+		double totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Samosa Pav";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 11;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Cheese Maggi";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 12;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Plain Maggi";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 13;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Veg Puff";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
-
+		quantity = 14;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 		foodName = "Chicken Puff";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 15;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Bombay Aloo Tikki Sandwich";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 21;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chilli Cheese Toast";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 33;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Garlic Bread with Cheese Dip";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 35;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Garlic Bread Party Pack";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 37;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Butter Chicken Steamed Bao";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Veg Steamed Pizza Bao";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 31;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Mini Butter Croissants";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Almond Croissant";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 21;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Butter Croissant";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 22;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Chole Kulche";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 23;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Paneer Makhani & Rice";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Butter Chicken & Rice";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 41;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Dal Makhani & Rice";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 42;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Rajma Masala & Rice";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 23;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Medu Vada Sambar Dip";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 45;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Rawa Upma";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 46;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Poha";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 47;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Masala Omelette Pav";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 57;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Vietnamese Cold Coffee";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 56;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Adrak Chai";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Hot Chocolate";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "French Vanilla Hot Chocolate";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 21;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Strawberry Shake";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 34;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Mixed Berry Shake";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Strawberry Lemonade";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Masala Chaas";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 82;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Triple Chocolate Mousse";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 23;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Kesari Rasmalai";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 54;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Tiramisu";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Angoori Gulab Jamun";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
-
+		quantity = 32;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 		foodName = "Choco Lava Cake";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 76;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Hot Americano";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 32;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Hot Milk";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "McCain French Fries";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 39;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "McCain Masala Fries";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 23;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "McCain Veggie Nuggets";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 45;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "McCain Chilli Chicken Sausages";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 37;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Prasuma Momos (Veg/Chicken";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 36;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "ITC Master Chef Chicken Seekh Kebab";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 10;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Venky's Chicken Nuggets";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 11;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Switz Paratha Puff";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Wow! Chicken Tikka Biryani";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 9;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Meatzza Chicken Jumbo Hotdog";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
-
+		quantity = 6;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
 		foodName = "Frozen Green Peas";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 7;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Frozen Sweet Corn";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 4;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Frozen Edamame";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 5;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Frozen Diced Mango";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 2;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 
 		foodName = "Smoked Paneer";
 		cost = Zepto.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
-
+		quantity = 3;
+		totalCost = Zepto.search(foodName , quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 	}
 }
