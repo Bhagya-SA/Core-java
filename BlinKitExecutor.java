aa0026e08475d8596cf5c5ce24dbbfa94ecc7302
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-02-23 09:47:18 +0530

    Update food items and their quantities in BlinKitExecutor

--- BlinKitExecutor.java
+++ BlinKitExecutor.java
@@ -2,221 +2,329 @@ class BlinKitExecutor {
 
 	public static void main(String[] blinkit) {
 		
-		String foodName = "Onions";
+		String foodName = "Bananas (Harichal/Robusta)";
 		double cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		int quantity = 2;
+		double totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
+		
+		foodName = "Onions";
+		cost = BlinKit.search(foodName);
+		quantity = 4;
+		totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Potatoes";
-	     cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+	    cost = BlinKit.search(foodName);
+		quantity = 7;
+		totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Tomatoes";
-		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		cost = BlinKit.search(foodName);
+		quantity = 9;
+		totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Coconut";
 		cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 8;
+		totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Green Chilli";
 		cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 5;
+		totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Garlic";
 		cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 3;
+		totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Coriander Leaves";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 3;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Curry Leaves";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 2;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Lemon";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 2;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Cucumber";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 3;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Capsicum";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 5;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Carrot";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 4;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Beans";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 4;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Spinach";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 4;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Broccoli";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 7;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Mushrooms";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 6;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Apple";
 		cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		quantity = 4;
+		totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Oranges";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 4;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Pomegranate";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 4;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Watermelon";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 2;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Grapes";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 6;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Mango";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 3;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Pre-cut Fruit Pack";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 7;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Amul Milk";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 3;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Mother Dairy Curd";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 7;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Britannia Bread";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 2;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Amul Butter";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 2;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Cheese Slices";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 5;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Mozzarella Cheese";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 7;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Yogurt";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 5;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Fresh Cream";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 4;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Condensed Milk";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 8;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Sliced Pav";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 9;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Bun";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 5;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Cream Rolls";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 3;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Sandwich Bread";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 8;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Lay’s India’s Magic Masala";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 7;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Kurkure Masala Munch";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 4;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Haldiram's Aloo Bhujia";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 3;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Haldiram's Bhujia Sev";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Haldiram's Punjabi Tadka";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 4;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Bikano Navratan Mixture";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 4;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Pringles";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 5;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Bingo! Mad Angles";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 3;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
-		foodName = "Pringles";
-		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
 		
 		foodName = "Kettle Studio Chips";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 6;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Cornitos Nachos";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quntity = 6;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Parle-G Biscuits";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 6;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Britannia Good Day";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 7;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Oreo Cookies";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 5;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Hide & Seek Chocolate Chips";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 5;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Marie Gold Biscuits";
+		quantity = 6;
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Britannia NutriChoice Digestive";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 8;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 		
 		foodName = "Trail Mix";
 		 cost = BlinKit.search(foodName);
-		System.out.println("The price of " + foodName + " is : " + cost);
+		 quantity = 3;
+		 totalCost = BlinKit.search(foodName,quantity);
+		System.out.println("The price of " + foodName + " is : " + cost + " quantity is " + quantity + " Total cost : " + totalCost);
 	
 	}
 
