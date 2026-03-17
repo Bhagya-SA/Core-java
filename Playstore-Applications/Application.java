245380a8f3054150b32bbae62e22d05ba0dbba67
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:51:45 +0530

    Update Application.java

--- Playstore-Applications/Application.java
+++ Playstore-Applications/Application.java
@@ -3,6 +3,12 @@ class Application {
     String appName;
     String appCategory;
 
+     public Application(int appId, String appName, String appCategory) {
+        this.appId = appId;
+        this.appName = appName;
+        this.appCategory = appCategory;
+    }
+
     public void displayAppInfo() {
         System.out.println("Application Id : " + this.appId);
         System.out.println("Application Name : " + this.appName);
