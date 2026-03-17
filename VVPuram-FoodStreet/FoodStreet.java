fb6a97c242b20c3935b82acfb7edb5fea2da665f
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 09:25:32 +0530

    Update FoodStreet.java

--- VVPuram-FoodStreet/FoodStreet.java
+++ VVPuram-FoodStreet/FoodStreet.java
@@ -4,6 +4,12 @@ class FoodStreet {
     String foodName;
     double price;
 
+    public FoodStreet(int foodId, String foodName, double price) {
+        this.foodId = foodId;
+        this.foodName = foodName;
+        this.price = price;
+    }
+
     public void displayFoodInfo() {
         System.out.println("Food Id : " + this.foodId);
         System.out.println("Food Name : " + this.foodName);
