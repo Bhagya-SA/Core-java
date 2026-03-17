149f6f6dc2d294199b1efcaeab41d21fd4a1c11d
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 09:13:19 +0530

    Update Package.java

--- Tourism-Package/Package.java
+++ Tourism-Package/Package.java
@@ -4,6 +4,12 @@ class Package {
     String destination;
     double price;
 
+     public Package(int packageId, String destination, double price) {
+        this.packageId = packageId;
+        this.destination = destination;
+        this.price = price;
+    }
+
     public void displayPackageInfo() {
         System.out.println("Package Id : " + this.packageId);
         System.out.println("Destination : " + this.destination);
