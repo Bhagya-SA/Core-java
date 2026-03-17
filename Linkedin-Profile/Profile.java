a9097a876f86056c1772020af1cd24a33ef1d73f
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:27:14 +0530

    Update Profile.java

--- Linkedin-Profile/Profile.java
+++ Linkedin-Profile/Profile.java
@@ -4,6 +4,12 @@ class Profile {
     String designation;
     String company;
 
+     public Profile(int profileId, String designation, String company) {
+        this.profileId = profileId;
+        this.designation = designation;
+        this.company = company;
+    }
+
     public void displayProfileInfo() {
         System.out.println("Profile Id : " + this.profileId);
         System.out.println("Designation : " + this.designation);
