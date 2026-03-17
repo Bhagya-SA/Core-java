1fcd384288cb73d39ddc243899dd7879ade7be2b
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 06:26:33 +0530

    Add constructor to CCD class

--- CCD-Coffee/CCD.java
+++ CCD-Coffee/CCD.java
@@ -5,6 +5,13 @@ class CCD {
     String location;
     Coffee coffee;
 
+    public CCD(int cafeId, String customerName, String location, Coffee coffee) {
+        this.cafeId = cafeId;
+        this.customerName = customerName;
+        this.location = location;
+        this.coffee = coffee;
+    }
+
     public void displayCafeInfo() {
         System.out.println("Cafe Id : " + this.cafeId);
         System.out.println("Customer Name : " + this.customerName);
