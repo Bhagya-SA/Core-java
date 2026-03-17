029dc346ff8f992a99954213b15248c9a6c5a7a2
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 09:28:48 +0530

    Update LazyPoolRide.java

--- Wonderla-Lazypool/LazyPoolRide.java
+++ Wonderla-Lazypool/LazyPoolRide.java
@@ -3,6 +3,12 @@ class LazyPoolRide {
     String rideName;
     String rideType;
 
+    public LazyPoolRide(int rideId, String rideName, String rideType) {
+        this.rideId = rideId;
+        this.rideName = rideName;
+        this.rideType = rideType;
+    }
+
     public void displayRideInfo() {
         System.out.println("Ride Id : " + this.rideId);
         System.out.println("Ride Name : " + this.rideName);
