48794562f27094bd471e65d507e6ec4c5bd3ca27
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 06:50:42 +0530

    Add constructor to Government class

--- Government-Policy/Government.java
+++ Government-Policy/Government.java
@@ -5,6 +5,13 @@ class Government {
     String country;
     Policy policy;
 
+     public Government(int governmentId, String governmentName, String country, Policy policy) {
+        this.governmentId = governmentId;
+        this.governmentName = governmentName;
+        this.country = country;
+        this.policy = policy;
+    }
+
     public void displayGovernmentInfo() {
         System.out.println("Government Id : " + this.governmentId);
         System.out.println("Government Name : " + this.governmentName);
