ee08b320994561a37777ea91d28ee85987957707
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 09:26:21 +0530

    Update VVPuram.java

--- VVPuram-FoodStreet/VVPuram.java
+++ VVPuram-FoodStreet/VVPuram.java
@@ -5,6 +5,14 @@ class VVPuram {
     String city;
     FoodStreet foodStreet;
 
+    public VVPuram(int streetId, String visitorName, String city, FoodStreet foodStreet) {
+        this.streetId = streetId;
+        this.visitorName = visitorName;
+        this.city = city;
+        this.foodStreet = foodStreet;
+    }
+
+
     public void displayStreetInfo() {
         System.out.println("Street Id : " + this.streetId);
         System.out.println("Visitor Name : " + this.visitorName);
