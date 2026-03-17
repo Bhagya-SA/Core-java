f88f14774f79b4270e19dce4c9078deccf1f7024
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 06:56:03 +0530

    Update Stock.java

--- Groww-Stock/Stock.java
+++ Groww-Stock/Stock.java
@@ -4,6 +4,12 @@ class Stock {
     String stockName;
     String sector;
 
+    public Stock(int stockId, String stockName, String sector) {
+        this.stockId = stockId;
+        this.stockName = stockName;
+        this.sector = sector;
+    }
+
     public void displayStockInfo() {
         System.out.println("Stock Id : " + this.stockId);
         System.out.println("Stock Name : " + this.stockName);
