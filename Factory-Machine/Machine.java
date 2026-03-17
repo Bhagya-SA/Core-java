098e2c0e2f45e3f6bfc3601fc2fd8b2c8be516b1
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 06:42:08 +0530

    Add constructor to Machine class
    Added constructor to initialize machine properties.

--- Factory-Machine/Machine.java
+++ Factory-Machine/Machine.java
@@ -3,6 +3,12 @@ class Machine {
     String machineName;
     String machineType;
 
+    public Machine(int machineId, String machineName, String machineType) {
+        this.machineId = machineId;
+        this.machineName = machineName;
+        this.machineType = machineType;
+    }
+
     public void displayMachineInfo() {
         System.out.println("Machine Id : " + this.machineId);
         System.out.println("Machine Name : " + this.machineName);
