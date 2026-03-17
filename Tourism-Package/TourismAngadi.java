3dcdf7a9e6617f1e977d69c932d37d029cadb2bb
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 09:13:58 +0530

    Update TourismAngadi.java

--- Tourism-Package/TourismAngadi.java
+++ Tourism-Package/TourismAngadi.java
@@ -5,6 +5,13 @@ class TourismAngadi {
     String email;
     Package pkg;
 
+    public TourismAngadi(int tourismId, String touristName, String email, Package pkg) {
+        this.tourismId = tourismId;
+        this.touristName = touristName;
+        this.email = email;
+        this.pkg = pkg;
+    }
+
     public void displayTourismInfo() {
         System.out.println("Tourism Id : " + this.tourismId);
         System.out.println("Tourist Name : " + this.touristName);
