1f28eb674c86443ec5ca1f639a5eb1e163a51f30
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 09:10:53 +0530

    Update TV.java

--- TV-Channel/TV.java
+++ TV-Channel/TV.java
@@ -5,6 +5,14 @@ class TV {
     String country;
     Channel channel;
 
+    public TV(int tvId, String tvName, String country, Channel channel) {
+        this.tvId = tvId;
+        this.tvName = tvName;
+        this.country = country;
+        this.channel = channel;
+    }
+
+    
     public void displayTVInfo() {
         System.out.println("TV Id : " + this.tvId);
         System.out.println("TV Name : " + this.tvName);
