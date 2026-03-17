c8b3f75a20721a42a7d7c3bd0d938a1f68af2491
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:55:32 +0530

    Update ProductionHouse.java

--- ProductionHouse-Movie/ProductionHouse.java
+++ ProductionHouse-Movie/ProductionHouse.java
@@ -5,6 +5,13 @@ class ProductionHouse {
     String location;
     Movie movie;
 
+    public ProductionHouse(int productionHouseId, String productionHouseName, String location, Movie movie) {
+        this.productionHouseId = productionHouseId;
+        this.productionHouseName = productionHouseName;
+        this.location = location;
+        this.movie = movie;
+    }
+
     public void displayProductionHouseInfo() {
         System.out.println("Production House Id : " + this.productionHouseId);
         System.out.println("Production House Name : " + this.productionHouseName);
