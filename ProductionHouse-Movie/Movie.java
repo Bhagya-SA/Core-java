5a4d71ad2e206b6c53f64ecb76f45576c0da861d
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:54:57 +0530

    Update Movie.java

--- ProductionHouse-Movie/Movie.java
+++ ProductionHouse-Movie/Movie.java
@@ -4,6 +4,12 @@ class Movie {
     String movieName;
     String genre;
 
+     public Movie(int movieId, String movieName, String genre) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.genre = genre;
+    }
+
     public void displayMovieInfo() {
         System.out.println("Movie Id : " + this.movieId);
         System.out.println("Movie Name : " + this.movieName);
