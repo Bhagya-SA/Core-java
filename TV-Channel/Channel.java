f75061bc17b25b2a85737fdcc0f967e4b64ab177
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 09:10:07 +0530

    Update Channel.java

--- TV-Channel/Channel.java
+++ TV-Channel/Channel.java
@@ -4,6 +4,12 @@ class Channel {
     String channelName;
     String channelType;
 
+    public Channel(int channelId, String channelName, String channelType) {
+        this.channelId = channelId;
+        this.channelName = channelName;
+        this.channelType = channelType;
+    }
+
     public void displayChannelInfo() {
         System.out.println("Channel Id : " + this.channelId);
         System.out.println("Channel Name : " + this.channelName);
