dcd09efd69a313dd4b64c08bff837825087476c1
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:18:51 +0530

    Update Team.java

--- Ipl-Player/Team.java
+++ Ipl-Player/Team.java
@@ -5,6 +5,14 @@ class Team {
     int totalMatches;
     Player player;
 
+     public Team(int teamId, String teamName, String location, int totalMatches, Player player) {
+        this.teamId = teamId;
+        this.teamName = teamName;
+        this.location = location;
+        this.totalMatches = totalMatches;
+        this.player = player;
+    }
+
     public void displayTeamInfo() {
         System.out.println("Team Id : " + this.teamId);
         System.out.println("Team Name : " + this.teamName);
