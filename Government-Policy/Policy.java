12ae71e55d87db5ee1261f2a905206cb1d0b73f0
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 06:51:39 +0530

    Add constructor to Policy class

--- Government-Policy/Policy.java
+++ Government-Policy/Policy.java
@@ -4,6 +4,12 @@ class Policy {
     String policyName;
     String policyType;
 
+     public Policy(int policyId, String policyName, String policyType) {
+        this.policyId = policyId;
+        this.policyName = policyName;
+        this.policyType = policyType;
+    }
+
     public void displayPolicyInfo() {
         System.out.println("Policy Id : " + this.policyId);
         System.out.println("Policy Name : " + this.policyName);
