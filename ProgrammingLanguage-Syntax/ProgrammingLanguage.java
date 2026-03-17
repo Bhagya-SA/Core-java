beb635de5ea27b3ebbeb76dc0a748d9baf0c2291
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:58:07 +0530

    Update ProgrammingLanguage.java

--- ProgrammingLanguage-Syntax/ProgrammingLanguage.java
+++ ProgrammingLanguage-Syntax/ProgrammingLanguage.java
@@ -5,6 +5,13 @@ class ProgrammingLanguage {
     String developer;
     Syntax syntax;
 
+     public ProgrammingLanguage(int languageId, String languageName, String developer, Syntax syntax) {
+        this.languageId = languageId;
+        this.languageName = languageName;
+        this.developer = developer;
+        this.syntax = syntax;
+    }
+
     public void displayProgrammingLanguageInfo() {
         System.out.println("Language Id : " + this.languageId);
         System.out.println("Language Name : " + this.languageName);
