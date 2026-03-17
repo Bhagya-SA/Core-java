e2af45e6484461e7b8cf038ac23b65a816ee06a0
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:19:48 +0530

    Update Player.java

--- Ipl-Player/Player.java
+++ Ipl-Player/Player.java
@@ -3,6 +3,12 @@ class Player {
     String playerName;
     String playerRole;
 
+    public Player(int playerId, String playerName, String playerRole) {
+        this.playerId = playerId;
+        this.playerName = playerName;
+        this.playerRole = playerRole;
+    }
+
     public void displayPlayerInfo() {
         System.out.println("Player Id : " + this.playerId);
         System.out.println("Player Name : " + this.playerName);
