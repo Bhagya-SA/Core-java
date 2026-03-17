6bb903c67ca5d43b2d762027b3088337912784f4
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:26:17 +0530

    Update Linkedin.java

--- Linkedin-Profile/Linkedin.java
+++ Linkedin-Profile/Linkedin.java
@@ -5,6 +5,13 @@ class Linkedin {
     String email;
     Profile profile;
 
+     public Linkedin(int linkedinId, String userName, String email, Profile profile) {
+        this.linkedinId = linkedinId;
+        this.userName = userName;
+        this.email = email;
+        this.profile = profile;
+    }
+
     public void displayLinkedinInfo() {
         System.out.println("LinkedIn Id : " + this.linkedinId);
         System.out.println("User Name : " + this.userName);
