328214139ccc7e78df494bb438bfafe3b79d2920
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 06:55:10 +0530

    Add constructor to Groww class

--- Groww-Stock/Groww.java
+++ Groww-Stock/Groww.java
@@ -5,6 +5,13 @@ class Groww {
     String accountType;
     Stock stock;
 
+     public Groww(int growwId, String userName, String accountType, Stock stock) {
+        this.growwId = growwId;
+        this.userName = userName;
+        this.accountType = accountType;
+        this.stock = stock;
+    }
+
     public void displayGrowwInfo() {
         System.out.println("Groww Id : " + this.growwId);
         System.out.println("User Name : " + this.userName);
