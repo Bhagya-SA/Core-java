52063a5bdbe0382f710db14363d856d0fcfbc184
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 09:05:24 +0530

    Update Salon.java

--- Salon-Service/Salon.java
+++ Salon-Service/Salon.java
@@ -5,6 +5,13 @@ class Salon {
     String location;
     Service service;
 
+     public Salon(int salonId, String customerName, String location, Service service) {
+        this.salonId = salonId;
+        this.customerName = customerName;
+        this.location = location;
+        this.service = service;
+    }
+
     public void displaySalonInfo() {
         System.out.println("Salon Id : " + this.salonId);
         System.out.println("Customer Name : " + this.customerName);
