86ddf63f83b20db899e707522a4293cea91e7b2a
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 09:22:21 +0530

    Update Earth.java

--- Universe-earth/Earth.java
+++ Universe-earth/Earth.java
@@ -4,6 +4,12 @@ class Earth {
     String earthName;
     String continent;
 
+    public Earth(int earthId, String earthName, String continent) {
+        this.earthId = earthId;
+        this.earthName = earthName;
+        this.continent = continent;
+    }
+
     public void displayEarthInfo() {
         System.out.println("Earth Id : " + this.earthId);
         System.out.println("Earth Name : " + this.earthName);
