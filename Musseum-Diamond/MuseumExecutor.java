d1a0cf25c01a7333fdfc36ecd73a1d98f25156ca
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:38:07 +0530

    Update MuseumExecutor.java

--- Musseum-Diamond/MuseumExecutor.java
+++ Musseum-Diamond/MuseumExecutor.java
@@ -1,6 +1,6 @@
 class MuseumExecutor {
     public static void main(String[] instruments) {
-        Museum museum1 = new Museum();
+       /* Museum museum1 = new Museum();
         museum1.museumId = 1;
         museum1.museumName = "Gem Museum, New York";
         museum1.location = "New York, USA";
@@ -238,6 +238,86 @@ class MuseumExecutor {
         diamond20.diamondName = "Blue Heart II";
         diamond20.diamondOrigin = "South Africa";
         museum20.diamond = diamond20;
-        museum20.displayMuseumInfo();
+        museum20.displayMuseumInfo(); */
+
+        Diamond d1 = new Diamond(101, "Hope Diamond", "India");
+        Museum m1 = new Museum(1, "Gem Museum, New York", "New York, USA", 50, d1);
+        m1.displayMuseumInfo();
+
+        Diamond d2 = new Diamond(102, "Koh-i-Noor", "India");
+        Museum m2 = new Museum(2, "London Diamond Museum", "London, UK", 45, d2);
+        m2.displayMuseumInfo();
+
+        Diamond d3 = new Diamond(103, "Regent Diamond", "India");
+        Museum m3 = new Museum(3, "Paris Gem Museum", "Paris, France", 40, d3);
+        m3.displayMuseumInfo();
+
+        Diamond d4 = new Diamond(104, "Cullinan Diamond", "South Africa");
+        Museum m4 = new Museum(4, "Amsterdam Diamond Museum", "Amsterdam, Netherlands", 35, d4);
+        m4.displayMuseumInfo();
+
+        Diamond d5 = new Diamond(105, "Orlov Diamond", "India");
+        Museum m5 = new Museum(5, "Moscow Diamond Fund", "Moscow, Russia", 60, d5);
+        m5.displayMuseumInfo();
+
+        Diamond d6 = new Diamond(106, "Sancy Diamond", "India");
+        Museum m6 = new Museum(6, "Dubai Gem Museum", "Dubai, UAE", 55, d6);
+        m6.displayMuseumInfo();
+
+        Diamond d7 = new Diamond(107, "Dresden Green", "India");
+        Museum m7 = new Museum(7, "Toronto Diamond Museum", "Toronto, Canada", 48, d7);
+        m7.displayMuseumInfo();
+
+        Diamond d8 = new Diamond(108, "Koh-i-Tan", "India");
+        Museum m8 = new Museum(8, "Tokyo Gem Museum", "Tokyo, Japan", 42, d8);
+        m8.displayMuseumInfo();
+
+        Diamond d9 = new Diamond(109, "Tiffany Diamond", "South Africa");
+        Museum m9 = new Museum(9, "Berlin Diamond Museum", "Berlin, Germany", 38, d9);
+        m9.displayMuseumInfo();
+
+        Diamond d10 = new Diamond(110, "Blue Heart", "South Africa");
+        Museum m10 = new Museum(10, "Bangkok Gem Museum", "Bangkok, Thailand", 44, d10);
+        m10.displayMuseumInfo();
+
+        Diamond d11 = new Diamond(111, "Regent II", "India");
+        Museum m11 = new Museum(11, "New York Gem Museum-2", "New York, USA", 50, d11);
+        m11.displayMuseumInfo();
+
+        Diamond d12 = new Diamond(112, "Koh-i-Noor II", "India");
+        Museum m12 = new Museum(12, "London Diamond Museum-2", "London, UK", 45, d12);
+        m12.displayMuseumInfo();
+
+        Diamond d13 = new Diamond(113, "Regent III", "India");
+        Museum m13 = new Museum(13, "Paris Gem Museum-2", "Paris, France", 40, d13);
+        m13.displayMuseumInfo();
+
+        Diamond d14 = new Diamond(114, "Cullinan II", "South Africa");
+        Museum m14 = new Museum(14, "Amsterdam Diamond Museum-2", "Amsterdam, Netherlands", 35, d14);
+        m14.displayMuseumInfo();
+
+        Diamond d15 = new Diamond(115, "Orlov II", "India");
+        Museum m15 = new Museum(15, "Moscow Diamond Fund-2", "Moscow, Russia", 60, d15);
+        m15.displayMuseumInfo();
+
+        Diamond d16 = new Diamond(116, "Sancy II", "India");
+        Museum m16 = new Museum(16, "Dubai Gem Museum-2", "Dubai, UAE", 55, d16);
+        m16.displayMuseumInfo();
+
+        Diamond d17 = new Diamond(117, "Dresden II", "India");
+        Museum m17 = new Museum(17, "Toronto Diamond Museum-2", "Toronto, Canada", 48, d17);
+        m17.displayMuseumInfo();
+
+        Diamond d18 = new Diamond(118, "Koh-i-Tan II", "India");
+        Museum m18 = new Museum(18, "Tokyo Gem Museum-2", "Tokyo, Japan", 42, d18);
+        m18.displayMuseumInfo();
+
+        Diamond d19 = new Diamond(119, "Tiffany II", "South Africa");
+        Museum m19 = new Museum(19, "Berlin Diamond Museum-2", "Berlin, Germany", 38, d19);
+        m19.displayMuseumInfo();
+
+        Diamond d20 = new Diamond(120, "Blue Heart II", "South Africa");
+        Museum m20 = new Museum(20, "Bangkok Gem Museum-2", "Bangkok, Thailand", 44, d20);
+        m20.displayMuseumInfo();
     }
 }
