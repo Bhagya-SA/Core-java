59acb13e32b6b063a4ee1f1f1bca14acaf0b18c5
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 09:29:51 +0530

    Update Wonderla.java

--- Wonderla-Lazypool/Wonderla.java
+++ Wonderla-Lazypool/Wonderla.java
@@ -5,6 +5,14 @@ class Wonderla {
     int totalRides;
     LazyPoolRide lazyPoolRide;
 
+    public Wonderla(int parkId, String parkName, String location, int totalRides, LazyPoolRide lazyPoolRide) {
+        this.parkId = parkId;
+        this.parkName = parkName;
+        this.location = location;
+        this.totalRides = totalRides;
+        this.lazyPoolRide = lazyPoolRide;
+    }
+
     public void displayParkInfo() {
         System.out.println("Park Id : " + this.parkId);
         System.out.println("Park Name : " + this.parkName);
