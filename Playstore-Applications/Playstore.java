bbb92548cb1f34615027f9523d71393bf372ce69
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:52:23 +0530

    Update Playstore.java

--- Playstore-Applications/Playstore.java
+++ Playstore-Applications/Playstore.java
@@ -5,6 +5,14 @@ class Playstore {
     int totalDownloads;
     Application application;
 
+     public Playstore(int storeId, String storeName, String location, int totalDownloads, Application application) {
+        this.storeId = storeId;
+        this.storeName = storeName;
+        this.location = location;
+        this.totalDownloads = totalDownloads;
+        this.application = application;
+    }
+
     public void displayStoreInfo() {
         System.out.println("Store Id : " + this.storeId);
         System.out.println("Store Name : " + this.storeName);
