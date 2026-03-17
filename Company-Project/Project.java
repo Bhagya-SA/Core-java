ebe4c206de040d3bcf930615596b939a1af05e44
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 06:34:09 +0530

    Add constructor to Project class

--- Company-Project/Project.java
+++ Company-Project/Project.java
@@ -4,6 +4,12 @@ class Project {
     String projectName;
     String projectType;
 
+    public Project(int projectId, String projectName, String projectType) {
+        this.projectId = projectId;
+        this.projectName = projectName;
+        this.projectType = projectType;
+    }
+
     public void displayProjectInfo() {
         System.out.println("Project Id : " + this.projectId);
         System.out.println("Project Name : " + this.projectName);
