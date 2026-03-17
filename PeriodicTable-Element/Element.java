3139bacb7a74d99e1ad26a5b33343603eb6b5ac5
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:48:19 +0530

    Update Element.java

--- PeriodicTable-Element/Element.java
+++ PeriodicTable-Element/Element.java
@@ -3,6 +3,12 @@ class Element {
     String elementName;
     String elementSymbol;
 
+     public Element(int elementId, String elementName, String elementSymbol) {
+        this.elementId = elementId;
+        this.elementName = elementName;
+        this.elementSymbol = elementSymbol;
+    }
+
     public void displayElementInfo() {
         System.out.println("Element Id : " + this.elementId);
         System.out.println("Element Name : " + this.elementName);
