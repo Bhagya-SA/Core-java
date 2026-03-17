f28e026a534497ea08bed629ef3585c7bb6b2b11
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 09:23:10 +0530

    Update Universe.java

--- Universe-earth/Universe.java
+++ Universe-earth/Universe.java
@@ -5,6 +5,13 @@ class Universe {
     String galaxy;
     Earth earth;
 
+     public Universe(int universeId, String universeName, String galaxy, Earth earth) {
+        this.universeId = universeId;
+        this.universeName = universeName;
+        this.galaxy = galaxy;
+        this.earth = earth;
+    }
+
     public void displayUniverseInfo() {
         System.out.println("Universe Id : " + this.universeId);
         System.out.println("Universe Name : " + this.universeName);
