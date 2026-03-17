dd0b92a053cce2569618c5f23d105daf3e42522d
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 06:44:40 +0530

    Update Formula1.java

--- Formula1_Franchisie/Formula1.java
+++ Formula1_Franchisie/Formula1.java
@@ -5,6 +5,14 @@ class Formula1 {
 	String carNumber;
 	String teamName;
 	Franchisie franchisie;
+
+	public Formula1(int id, String driverName, String carNumber, String teamName, Franchisie franchisie) {
+        this.id = id;
+        this.driverName = driverName;
+        this.carNumber = carNumber;
+        this.teamName = teamName;
+        this.franchisie = franchisie;
+    }
 	
 	public void displayFomula1Info() {
 		System.out.println("Id is : " + this.id);
