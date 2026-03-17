8f63ee4bf3ac9ede38c640c6f3a3a9af4c42eeb0
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 06:27:26 +0530

    Add constructor to Coffee class

--- CCD-Coffee/Coffee.java
+++ CCD-Coffee/Coffee.java
@@ -4,6 +4,12 @@ class Coffee {
     String coffeeName;
     double price;
 
+    public Coffee(int coffeeId, String coffeeName, double price) {
+        this.coffeeId = coffeeId;
+        this.coffeeName = coffeeName;
+        this.price = price;
+    }
+
     public void displayCoffeeInfo() {
         System.out.println("Coffee Id : " + this.coffeeId);
         System.out.println("Coffee Name : " + this.coffeeName);
