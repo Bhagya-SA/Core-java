37085bd9b6ea3936434a93898610778e0ba1f73f
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:49:02 +0530

    Update PeriodicTable.java

--- PeriodicTable-Element/PeriodicTable.java
+++ PeriodicTable-Element/PeriodicTable.java
@@ -5,6 +5,14 @@ class PeriodicTable {
     int totalElements;
     Element element;
 
+     public PeriodicTable(int tableId, String tableName, String discoveryLocation, int totalElements, Element element) {
+        this.tableId = tableId;
+        this.tableName = tableName;
+        this.discoveryLocation = discoveryLocation;
+        this.totalElements = totalElements;
+        this.element = element;
+    }
+
     public void displayTableInfo() {
         System.out.println("Table Id : " + this.tableId);
         System.out.println("Table Name : " + this.tableName);
