c8cc501c9e779e6ba0d7ea37ff08a220cf005a81
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 09:06:26 +0530

    Update Service.java

--- Salon-Service/Service.java
+++ Salon-Service/Service.java
@@ -4,6 +4,12 @@ class Service {
     String serviceName;
     double price;
 
+    public Service(int serviceId, String serviceName, double price) {
+        this.serviceId = serviceId;
+        this.serviceName = serviceName;
+        this.price = price;
+    }
+
     public void displayServiceInfo() {
         System.out.println("Service Id : " + this.serviceId);
         System.out.println("Service Name : " + this.serviceName);
