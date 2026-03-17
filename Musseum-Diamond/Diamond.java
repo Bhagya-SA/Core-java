992ac477cd75037239644f320c9aad0e1a54c9c8
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:34:11 +0530

    Update Diamond.java

--- Musseum-Diamond/Diamond.java
+++ Musseum-Diamond/Diamond.java
@@ -3,6 +3,12 @@ class Diamond {
     String diamondName;
     String diamondOrigin;
 
+    public Diamond(int diamondId, String diamondName, String diamondOrigin) {
+        this.diamondId = diamondId;
+        this.diamondName = diamondName;
+        this.diamondOrigin = diamondOrigin;
+    }
+
     public void displayDiamondInfo() {
         System.out.println("Diamond Id : " + this.diamondId);
         System.out.println("Diamond Name : " + this.diamondName);
