7c5f6e06feca6187b4d0e2418fb3020657892b5a
Bhagyashree S A <bhagyaanjaneya@gmail.com>
2026-03-17 07:36:23 +0530

    Update Museum.java

--- Musseum-Diamond/Museum.java
+++ Musseum-Diamond/Museum.java
@@ -5,6 +5,13 @@ class Museum {
     int totalDiamonds;
     Diamond diamond;
 
+     public Museum(int museumId, String museumName, String location, int totalDiamonds, Diamond diamond) {
+        this.museumId = museumId;
+        this.museumName = museumName;
+        this.location = location;
+        this.totalDiamonds = totalDiamonds;
+        this.diamond diamond;
+
     public void displayMuseumInfo() {
         System.out.println("Museum Id : " + this.museumId);
         System.out.println("Museum Name : " + this.museumName);
